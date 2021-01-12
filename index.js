5cb61e21901633f5d73cbbfd634aeb7fa9b7e855
sangzun.han <hsjun1996@naver.com>
2021-01-13 01:55:57 +0900

    [feat] login

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const bodyParser = require("body-parser");
-
+const cookieParser = require("cookie-parser");
 const config = require("./config/key");
 
 const { User } = require("./models/User");
@@ -12,6 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //application/json
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require("mongoose");
 mongoose
@@ -29,13 +30,42 @@ app.get("/", (req, res) => res.send("Hello World"));
 app.post("/register", (req, res) => {
   // 회원가입시 필요한 정보 client에서 가져오면
   // DB에 저장
-
   const user = new User(req.body);
 
   user.save((err, doc) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).json({
       success: true,
+    });
+  });
+});
+
+app.post("/login", (req, res) => {
+  // 요청이메일 DB에서찾기
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다.",
+      });
+    }
+    // 요청된 이메일 DB에 존재한다면 비밀번호 확인
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({
+          loginSuccess: false,
+          message: "비밀번호가 틀렸습니다.",
+        });
+    });
+    // 비밀번호 맞다면 token생성
+    user.generateToken((err, user) => {
+      if (err) return res.status(400).send(err);
+
+      // token 저장
+      res.cookie("x_auth", user.token).status(200).json({
+        loginSuccess: true,
+        userId: user._id,
+      });
     });
   });
 });
